7ef6bf83364a722891626cb3b2af1c3037c3b242
victormusembi11 <victor.musembi11@gmail.com>
2023-11-04 22:07:06 +0300

    feat: app options

---
+++ main.py
@@ -0,0 +1,20 @@
+from app.view import create_contact, view_contacts, delete_contact
+
+while True:
+    print("1. View Contacts")
+    print("2. Add Contact")
+    print("3. Delete Contact")
+    print("4. Exit")
+
+    choice = input("Select an option: ")
+
+    if choice == "1":
+        view_contacts()
+    elif choice == "2":
+        create_contact()
+    elif choice == "3":
+        delete_contact()
+    elif choice == "4":
+        break
+    else:
+        print("Invalid option. Please choose 1, 2, 3, or 4.")
